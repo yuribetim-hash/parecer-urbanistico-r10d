d4af2bbcc4fb37719da28bc32845b02ceffea595
yuribetim-hash <yuribetim@gmail.com>
2026-04-09 14:13:10 -0300

    app.py

--- app.py
+++ app.py
@@ -233,6 +233,7 @@ if st.button("Gerar Parecer"):
     st.download_button(
         label="📄 Baixar Parecer (.docx)",
         data=arquivo,
-        file_name="parecer_urbanistico.docx",
+        protocolo_limpo = protocolo.replace("/", "-") if protocolo else "sem_protocolo"
+        file_name=f"parecer_{protocolo_limpo}.docx"
         mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
     )
